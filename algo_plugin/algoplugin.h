09d36218de8640fe12b41ff20d2037d5366a319e
lingtianyulong <lingtianyulong@126.com>
2026-01-16 12:30:32 +0800

    1. 上传 C++ algoplugin 相关的文件;

---
+++ algo_plugin/algoplugin.h
@@ -0,0 +1,13 @@
+#ifndef ALGOPLUGIN_H
+#define ALGOPLUGIN_H
+
+namespace talos::plugin {
+
+// class AlgoPlugin {
+// public:
+//   virtual ~AlgoPlugin() = default;
+// };
+
+} // namespace talos::plugin
+
+#endif // ALGOPLUGIN_H
---
+++ algo_plugin/rust_core/algo_base_tool.h
@@ -0,0 +1,31 @@
+#ifndef ALGO_BASE_TOOL_H
+#define ALGO_BASE_TOOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ToolObject;
+
+using ToolNameFunc = const char *(*)(ToolObject *);
+using ToolInitFunc = bool (*)(ToolObject *);
+using ToolRunFunc = int (*)(ToolObject *);
+using ToolDestroyFunc = void (*)(ToolObject *);
+
+struct ToolVTable {
+  ToolNameFunc name;
+  ToolInitFunc init;
+  ToolRunFunc run;
+  ToolDestroyFunc destroy;
+};
+
+struct Tool {
+  ToolObject *obj;
+  ToolVTable *vtable;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#define ALGO_BASE_TOOL_H
