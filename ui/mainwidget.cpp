db37f95747fe39ca7f0ecf89d610927620595468
lingtianyulong <lingtianyulong@126.com>
2026-01-23 15:47:35 +0800

    1. 将 dock 样式调整到窗口的整体风格, 但悬浮窗口还未调整;

--- ui/mainwidget.cpp
+++ ui/mainwidget.cpp
@@ -350,23 +350,67 @@ void MainWidget::initUI() {
 
   // 使用 IconProvider 全局设置图标，解决 QSS 中的 Unknown 警告
   ads::CIconProvider &provider = ads::CDockManager::iconProvider();
-  provider.registerCustomIcon(ads::TabCloseIcon,
-                              QIcon(":/ads/images/close-button.svg"));
-  provider.registerCustomIcon(ads::DockAreaCloseIcon,
-                              QIcon(":/ads/images/close-button.svg"));
 
-  // 设置 Pin 按钮图标及其状态
+  // 辅助函数：将 SVG 图标颜色修改为白色
+  auto createWhiteIcon = [](const QString &path) -> QIcon {
+    QFile file(path);
+    if (file.open(QIODevice::ReadOnly)) {
+      QByteArray data = file.readAll();
+      // 替换颜色值 #000000 为 #FFFFFF
+      data.replace("#000000", "#FFFAFA");
+      data.replace("fill:#000000", "fill:#FFFAFA");
+      data.replace("stroke:#000000", "stroke:#FFFAFA");
+      data.replace("style=\"-inkscape-stroke:none\"",
+                   "style=\"fill:#FFFAFA;-inkscape-stroke:none\"");
+
+      QPixmap pixmap;
+      pixmap.loadFromData(data);
+      return QIcon(pixmap);
+    }
+    return QIcon(path);
+  };
+
+  provider.registerCustomIcon(ads::TabCloseIcon,
+                              createWhiteIcon(":/ads/images/close-button.svg"));
+  provider.registerCustomIcon(ads::DockAreaCloseIcon,
+                              createWhiteIcon(":/ads/images/close-button.svg"));
+
   QIcon pinIcon;
-  pinIcon.addFile(":/ads/images/vs-pin-button.svg", QSize(), QIcon::Normal,
-                  QIcon::Off);
-  pinIcon.addFile(":/ads/images/vs-pin-button-pinned.svg", QSize(),
-                  QIcon::Normal, QIcon::On);
+  // 手动处理 Pin 图标的白色版本
+  auto createWhitePinIcon = [&](const QString &path) -> QPixmap {
+    QFile file(path);
+    if (file.open(QIODevice::ReadOnly)) {
+      QByteArray data = file.readAll();
+      data.replace("#000000", "#FFFAFA");
+      data.replace("fill:#000000", "fill:#FFFAFA");
+      data.replace("stroke:#000000", "stroke:#FFFAFA");
+      // 针对 vs-pin-button.svg 缺少 fill 属性的情况进行特殊处理
+      data.replace("style=\"-inkscape-stroke:none\"",
+                   "style=\"fill:#FFFAFA;-inkscape-stroke:none\"");
+      QPixmap pixmap;
+      pixmap.loadFromData(data);
+      return pixmap;
+    }
+    return QPixmap(path);
+  };
+
+  QPixmap unpinned = createWhitePinIcon(":/ads/images/vs-pin-button.svg");
+  QPixmap pinned = createWhitePinIcon(":/ads/images/vs-pin-button-pinned.svg");
+  pinIcon.addPixmap(unpinned, QIcon::Normal, QIcon::Off);
+  pinIcon.addPixmap(unpinned, QIcon::Active, QIcon::Off);
+  pinIcon.addPixmap(pinned, QIcon::Normal, QIcon::On);
+  pinIcon.addPixmap(pinned, QIcon::Active, QIcon::On);
   provider.registerCustomIcon(ads::AutoHideIcon, pinIcon);
 
-  provider.registerCustomIcon(ads::DockAreaMenuIcon,
-                              QIcon(":/ads/images/tabs-menu-button.svg"));
-  provider.registerCustomIcon(ads::DockAreaUndockIcon,
-                              QIcon(":/ads/images/detach-button.svg"));
+  provider.registerCustomIcon(
+      ads::DockAreaMenuIcon,
+      createWhiteIcon(":/ads/images/tabs-menu-button.svg"));
+  provider.registerCustomIcon(
+      ads::DockAreaUndockIcon,
+      createWhiteIcon(":/ads/images/detach-button.svg"));
+  provider.registerCustomIcon(
+      ads::DockAreaMinimizeIcon,
+      createWhiteIcon(":/ads/images/minimize-button.svg"));
 
   ads::CDockManager *manager = new ads::CDockManager(this);
   manager->setStyleSheet(""); // 清除 ADS 默认样式，允许全局 QSS 生效
